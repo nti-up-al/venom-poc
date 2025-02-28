0714fbf0469c053a37b2d5acbb92adc7f19b679c
Kayê Holanda <kaye.holanda@gmail.com>
2025-02-28 11:09:59 -0300

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+const venom = require('venom-bot');
+
+venom
+  .create({
+    session: 'session-name' //name of session
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+async function start(client) {
+  await client
+    .sendText('numero-de-telefone@c.us', '👋 Hello from venom!')
+    .then((result) => {
+      console.log('Result: ', result); //return object success
+    })
+    .catch((erro) => {
+      console.error('Error when sending: ', erro); //return object error
+    });
+}
