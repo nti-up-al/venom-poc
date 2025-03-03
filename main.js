31c6155ca272495a3a3f6aa5eccc64aeff9f569a
Diego <diego.almeida@gmail.com>
2025-03-03 00:06:56 -0300

    teste poc

--- main.js
+++ main.js
@@ -1,21 +1,62 @@
 const venom = require('venom-bot');
-
+const axios = require('axios/dist/node/axios.cjs'); // node
 venom
-  .create({
-    session: 'session-name' //name of session
-  })
+.create(
+  //session
+  'sessionName', //Pass the name of the client you want to start the bot
+  //catchQR
+  (base64Qrimg, asciiQR, attempts, urlCode) => {
+    console.log('Number of attempts to read the qrcode: ', attempts);
+    console.log('Terminal qrcode: ', asciiQR);
+    console.log('base64 image string qrcode: ', base64Qrimg);
+    console.log('urlCode (data-ref): ', urlCode);
+  },
+  // statusFind
+  (statusSession, session) => {
+    console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
+    //Create session wss return "serverClose" case server for close
+    console.log('Session name: ', session);
+  },
+  // options
+  {
+   headless: 'new', // you should no longer use boolean false or true, now use false, true or 'new' learn more https://developer.chrome.com/articles/new-headless/
+  },
+
+  // BrowserInstance
+  (browser, waPage) => {
+    console.log('Browser PID:', browser.process().pid);
+    waPage.screenshot({ path: 'screenshot.png' });
+  }
+)
   .then((client) => start(client))
   .catch((erro) => {
     console.log(erro);
   });
 
 async function start(client) {
-  await client
-    .sendText('numero-de-telefone@c.us', '👋 Hello from venom!')
-    .then((result) => {
-      console.log('Result: ', result); //return object success
-    })
-    .catch((erro) => {
-      console.error('Error when sending: ', erro); //return object error
-    });
+  client.onMessage((message) => {
+    if (message.body === 'agenda-estudante' && message.isGroupMsg === false) {
+      client
+        .then((result) => {
+          axios.get('http://localhost:8080/cremessage/'+response.from)
+          .then(function (response) {
+            session.push(response.from);
+            return result.from;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+          
+        }).sendText((result.from, result.message)) = {
+            
+        }
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }else if(checkSession(message.from) );
+
+  });
+
+ 
 }
+
